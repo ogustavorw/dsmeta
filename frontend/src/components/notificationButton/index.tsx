300c0317112e794e15823da60893a3663852fdf3
ogustavorw <gustavowoicekoscki@hotmail.com>
2022-07-16 14:46:49 -0300

    tsconfig.json - types

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
-
 import './styles.css';
 
 type Props = {
